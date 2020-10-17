1524bffa87f8055fcdb88682ebbd8417ba10b88c
skorobahatko <maxim.skorobagatko@gmail.com>
2020-10-17 19:07:50 +0300

    changed gitignore, added MainPage

--- src/components/main-page/MainPage.js
+++ src/components/main-page/MainPage.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 const MainPage = () => {
+    const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+    console.log (apiKey);
+
+    const [currentWeather, changeCurrentWeater] = useState({});
+
+    const getJson = () => {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=Lviv&appid=${apiKey}`)
+            .then(data => data.json())
+            .then(data => changeCurrentWeater(data))
+            .catch(e => console.log(e));
+    };
+    useEffect(() => {
+        getJson();
+    },[]);
+    // getJson();
 
     return (
-        <div>hello weather site</div>
+        <div>
+            {JSON.stringify(currentWeather)}
+        </div>
     )
 };
 export default MainPage;
